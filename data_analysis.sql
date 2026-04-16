efacce9c1d06acc80868cb348a1bdbc7ee91dc9d
Jose Hernandez <120428012+JoseAngelMay@users.noreply.github.com>
2026-04-15 18:57:44 -0700

    Update data_analysis.sql

--- data_analysis.sql
+++ data_analysis.sql
@@ -62,9 +62,34 @@ JOIN products p ON p.product_id = oi.product_id
 GROUP BY (product_category_name, SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 6, 2))
 ORDER BY month ASC, category_sales DESC;
 
-SELECT SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 6, 2) AS month, COUNT(*) AS sales
+SELECT * FROM product_category_name_translation;
+
+SELECT pcnt.category_name_en, SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 6, 2) AS month,
+	   SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 1, 4) AS year, COUNT(p.product_category_name) AS category_sales
 FROM order_items oi
 JOIN orders o ON oi.order_id = o.order_id
 JOIN products p ON p.product_id = oi.product_id
-GROUP BY (SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 6, 2))
-ORDER BY month ASC;
+JOIN product_category_name_translation pcnt ON p.product_category_name = pcnt.category_name
+GROUP BY (pcnt.category_name_en, SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 6, 2), SUBSTR(CAST(o.order_purchase_timestamp AS TEXT), 1, 4))
+ORDER BY year ASC, month ASC, category_sales DESC;
+
+SELECT * FROM geolocation; -- zip_code_prefix, lat, lng, city, state
+SELECT * FROM customers; -- id, zip_code_prefix, city, state
+SELECT * FROM products; --product_id
+SELECT * FROM order_items; -- order_id, product_id
+SELECT * FROM orders; -- order_id, customer_id
+
+
+SELECT c.id AS customer_id, COUNT(o.customer_id) customer_purchases, g.lat AS latitude, g.lng AS longitude
+FROM geolocation g
+JOIN customers c ON g.zip_code_prefix = c.zip_code_prefix AND g.state = c.state AND g.city = c.city
+JOIN orders o ON o.customer_id = c.id
+JOIN order_items oi ON oi.order_id = o.order_id
+JOIN products p ON p.product_id = oi.product_id
+GROUP BY c.id, g.lat, g.lng
+ORDER BY COUNT(o.customer_id) DESC;
+
+
+SELECT * 
+FROM orders
+WHERE customer_id = 'fc3d1daec319d62d49bfb5e1f83123e9'; -- fc3d1daec319d62d49bfb5e1f83123e9
